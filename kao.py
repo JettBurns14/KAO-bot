12ca398d9a346e739d6d719abf7d15d9aac1f89b
Jett <jettburns11@icloud.com>
2017-06-25 09:49:05 -0500

    Update kao.py

--- kao.py
+++ kao.py
@@ -18,7 +18,7 @@ async def on_message(message):
 		await client.send_message(message.channel, '**Help**\n-Command one\n-Command two\n-Command three')
 
 		
-@client.listener('after_server_stop'):
+@client.listener('after_server_stop')
     await session.close()
 
 		
