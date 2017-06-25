d4e49c6ac7f55778ff6e64b0b00f1e6287cc0c40
Jett <jettburns11@icloud.com>
2017-06-25 08:17:10 -0500

    Add files via upload

---
+++ kao.py
@@ -0,0 +1,21 @@
+import discord
+import asyncio
+import logging
+
+#logging.basicConfig(level=logging.INFO)
+
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+	print('Logged in as')
+	print(client.user.name)
+
+@client.event
+async def on_message(message):
+	if message.content.startswith('-help'):
+		await client.send_message(message.channel, '**Help**\n```Command one\nCommand two\nCommand three```')
+
+
+client.run('MzI3MjM5ODg5Mzc0NDEyODEw.DCydvQ.UKrI7CkR7N7uUTOJOY___pSi-iU')
