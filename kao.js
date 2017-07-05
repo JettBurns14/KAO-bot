57ead07bdccdf270c42de6be9df10016905e44f1
Jett <jettburns11@icloud.com>
2017-07-04 20:09:36 -0500

    Update kao.js

--- kao.js
+++ kao.js
@@ -104,7 +104,7 @@ client.on('message', message => {
     if (command === 'info') {
         let embed = new Discord.RichEmbed();
         
-		console.log(new Object(olympians).length)
+		console.log(JSON.parse(olympians).TRedL)
 		
         // Loops through `olympians`, checking for a match with the argument.
 		/*
