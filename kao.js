92a387af01c2587d99d9e7459fba8d6418108df7
Jett <jettburns11@icloud.com>
2017-07-04 21:02:15 -0500

    Update kao.js

--- kao.js
+++ kao.js
@@ -112,11 +112,11 @@ client.on('message', message => {
             for (var j = 0; j < json.TRedL.length; j++) {
 				var current = json[Object.keys(json)[i]][j];
 				console.log(current);
-				if (current === args || current.toLowerCase() === args) {
+				/*if (current === args || current.toLowerCase() === args) {
 					embed.setColor(colors[i]);
 					embed.addField(current + "'s Info", "**" + current + "'s** events are:\n```" + json[Object.keys(json)[i]][j].events + "```");
 					message.channel.sendEmbed(embed);
-                }
+                }*/
             }
         }
 		/*
