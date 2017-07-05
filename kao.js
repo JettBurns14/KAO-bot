5d0cff415649ce6b51137c6dd763b754e60fa460
Jett <jettburns11@icloud.com>
2017-07-04 21:36:36 -0500

    Updated kao.js
    Fixed the uptime.

--- kao.js
+++ kao.js
@@ -69,6 +69,9 @@ client.on('ready', () => {
     client.user.setUsername('KAO bot');
     console.log('I am ready Jett!');
 
+	setInterval(function() {
+		totalTime += 1;
+	}, 1000);
 	request('https://www.khanacademy.org/api/labs/scratchpads/5991458534129664', function(error, response, body) {
 		olympians = JSON.parse(body).revision.code;
 	});
